ba136545f17df36be4fb7e4463b3f7cada76c543
Raj Aryan Singh <rajlysm15@gmail.com>
2023-09-18 15:13:41 +0530

    serving HTML

--- ws-server/index.js
+++ ws-server/index.js
@@ -3,9 +3,13 @@ const app = express();
 const http = require ("http")
 const server  = http.createServer(app); 
 
+// app.get('/', (req,res) => {
+//     res.send('<h1> web socket </h1>')
+// });
+
 app.get('/', (req,res) => {
-    res.send('<h1> web socket </h1>')
-});
+    res.sendFile (__dirname + '/index.html')
+})
 
 server.listen(3000, ()=> {
     console.log('listening on port 3000');
