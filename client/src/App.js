b2ab99bd0988183f105f929e839e3b946da1516b
Raj Aryan Singh <rajlysm15@gmail.com>
2023-09-19 23:24:56 +0530

    getting the id and messgae

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
-import React,{useState , useRef, useEffect} from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styled from "styled-component";
-import io from "socket.io-client"
+import io from "socket.io-client";
 
-const App  =() =>  {
- const [uourId , setYourId] = useState ()
- const [messages , setMessages] = useState ([])
- const [message , setMessage] = useState ()
- 
- const socketRef = useRef();
+const App = () => {
+  const [uourId, setYourId] = useState();
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState();
 
- useEffect(()=>{
-  socket.current = io.connect('/');
- },
- [])
+  const socketRef = useRef();
 
-}
+  useEffect(() => {
+    socket.current = io.connect("/");
+    socketRef.current.on("your id",id =>{
+      setYourId(id);
+    })
+    socketRef.current.on ("message",(msg)=>{
+      receivedMessage(message)
+    })
+
+  }, []);
+  function receivedMessage (message) {
+    setMessage(oldMsgs =>[...oldMsgs, message])
+  }
+};
