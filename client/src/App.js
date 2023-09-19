25aedc22e08d69eaddd4de7498e27b509c2c2bb9
Raj Aryan Singh <rajlysm15@gmail.com>
2023-09-19 23:24:32 +0530

    to establish web socket connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React,{useState , useRef} from "react";
+import React,{useState , useRef, useEffect} from "react";
 import styled from "styled-component";
 import io from "socket.io-client"
 
@@ -6,4 +6,12 @@ const App  =() =>  {
  const [uourId , setYourId] = useState ()
  const [messages , setMessages] = useState ([])
  const [message , setMessage] = useState ()
+ 
+ const socketRef = useRef();
+
+ useEffect(()=>{
+  socket.current = io.connect('/');
+ },
+ [])
+
 }
