6e74912d6e01c5ec3c424207ae33a16d5e8f4d6c
Raj Aryan Singh <rajlysm15@gmail.com>
2023-09-19 23:08:48 +0530

    usestate rook used, for setting the id, messages and individual message

--- client/src/App.js
+++ client/src/App.js
@@ -1,2 +1,9 @@
 import React,{useState , useRef} from "react";
 import styled from "styled-component";
+import io from "socket.io-client"
+
+const App  =() =>  {
+ const [uourId , setYourId] = useState ()
+ const [messages , setMessages] = useState ([])
+ const [message , setMessage] = useState ()
+}
